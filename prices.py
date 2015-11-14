c965d831f045c42c961ed09dccb5143aab7c1174
will-cromar <wcromar@knights.ucf.edu>
2015-11-14 14:05:01 -0500

    Changed stuff

--- prices.py
+++ prices.py
@@ -5,7 +5,7 @@ COPPER_PRICES_NAME = "imf_copper_monthly"
 
 prices = None
 if not util.pickleExists(COPPER_PRICES_NAME):
-    prices = Quandl.Quandl.get("ODA/PCOPP_USD")
+    prices = Quandl.get("ODA/PCOPP_USD", returns="numpy", authtoken="xx_T2u2fsQ_MjyZjTb6E")
     util.savePickle(prices, COPPER_PRICES_NAME)
 else:
     prices = util.getMostRecentPickle(COPPER_PRICES_NAME)
