26ad3fb5e0b6c3031f47598fe776c6f51b4bef4b
will-cromar <wcromar@knights.ucf.edu>
2015-11-17 16:12:00 -0500

    Changed name of copper_prices.py. Made function to get any arbitrary stock's data

---
+++ price_parsing.py
@@ -0,0 +1,53 @@
+import util
+import Quandl
+
+COPPER_PRICES_NAME = "imf_copper_monthly"
+STOCK_DATA_SOURCE = "YAHOO/" # Yahoo seems like a good source
+
+#Deprecated -- Remove before turning in
+def getCopperPrices(update=False):
+    prices = None
+    if update or not util.pickleExists(COPPER_PRICES_NAME):
+        prices = Quandl.get("ODA/PCOPP_USD", returns="numpy", authtoken="xx_T2u2fsQ_MjyZjTb6E")
+        util.savePickle(prices, COPPER_PRICES_NAME)
+    else:
+        prices = util.getMostRecentPickle(COPPER_PRICES_NAME)
+
+    return prices
+#Also deprecated
+def preprocessCopperPrices(prices):
+    X = [[entry[0].toordinal()] for entry in prices]
+    y = [float(entry[1]) for entry in prices]
+
+    return X, y
+
+def getStockPrices(ticker, frequency="monthly", update=False):
+    name = ticker + "_" + frequency
+    prices = None
+    if update or not util.pickleExists(name):
+        prices = Quandl.get(STOCK_DATA_SOURCE + ticker, collapse=frequency, authtoken="xx_T2u2fsQ_MjyZjTb6E")
+        util.savePickle(prices, name)
+    else:
+        prices = util.getMostRecentPickle(name)
+
+    return prices.get("Close")
+
+def preprocessStocks(priceData):
+    timestamps = [[row[0].toordinal()] for row in priceData.iteritems()]
+    prices = [float(row[1]) for row in priceData.iteritems()]
+
+    return timestamps, prices
+
+# Try out the new stock price grabber/preprocessor with google's data
+def test():
+    data = getStockPrices("GOOG", frequency="daily")
+    times, prices = preprocessStocks(data)
+    print times
+    print prices
+
+    from regression_graphs import graphRegressionsOverTime
+    graphRegressionsOverTime(times, prices, [])
+
+test()
+
+#print data.data
