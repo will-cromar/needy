653b396aeafc8dd45a4a789a848f006b91c4ff0a
will-cromar <wcromar@knights.ucf.edu>
2015-11-21 15:38:48 -0500

    Made some changes

--- price_parsing.py
+++ price_parsing.py
@@ -1,31 +1,24 @@
 import util
 import Quandl
 
-COPPER_PRICES_NAME = "imf_copper_monthly"
 STOCK_DATA_SOURCE = "YAHOO/" # Yahoo seems like a good source
-
-#Deprecated -- Remove before turning in
-def getCopperPrices(update=False):
-    prices = None
-    if update or not util.pickleExists(COPPER_PRICES_NAME):
-        prices = Quandl.get("ODA/PCOPP_USD", returns="numpy", authtoken="xx_T2u2fsQ_MjyZjTb6E")
-        util.savePickle(prices, COPPER_PRICES_NAME)
-    else:
-        prices = util.getMostRecentPickle(COPPER_PRICES_NAME)
-
-    return prices
-#Also deprecated
-def preprocessCopperPrices(prices):
-    X = [[entry[0].toordinal()] for entry in prices]
-    y = [float(entry[1]) for entry in prices]
-
-    return X, y
 
 def getStockPrices(ticker, frequency="monthly", update=False):
     name = ticker + "_" + frequency
     prices = None
     if update or not util.pickleExists(name):
         prices = Quandl.get(STOCK_DATA_SOURCE + ticker, collapse=frequency, authtoken="xx_T2u2fsQ_MjyZjTb6E")
+        util.savePickle(prices, name)
+    else:
+        prices = util.getMostRecentPickle(name)
+
+    return prices.get("Close")
+
+def getStockPriceDifferentials(ticker, frequency="monthly", update=False):
+    name = ticker + "_differentials_" + frequency
+    prices = None
+    if update or not util.pickleExists(name):
+        prices = Quandl.get(STOCK_DATA_SOURCE + ticker, collapse=frequency, transform="rdiff", authtoken="xx_T2u2fsQ_MjyZjTb6E")
         util.savePickle(prices, name)
     else:
         prices = util.getMostRecentPickle(name)
