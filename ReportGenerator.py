20207de68b8928d3c5394984b787f4fb8444f6c5
tylervanharen <tyler.vanharen@gmail.com>
2015-12-03 21:23:46 -0500

    mostly sorta fixed

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -17,22 +17,34 @@ import requests
 def genReport(company, runs, newsCount):
     print "starting generation with ", runs, " runs and ", newsCount, " articles."
     report = canvas.Canvas(company+".pdf", pagesize=letter)
+    #Embedding the font into the pdf so it can render
     ttfFile = "Humor-Sans-1.0.ttf"
     pdfmetrics.registerFont(TTFont("HumorSans",  ttfFile))
+
     width, height = letter
+
+    #Drawing blue background on pdf
     report.setFillColor(HexColor('#345592'))
     report.setStrokeColor(HexColor('#91A2C4'))
     report.rect(0, 0, width, height, fill=True)
     report.setFillColor(HexColor('#91A2C4'))
+
+    #Writing heading to top of PDF with lines beneath for aesthetics
     report.setFont("HumorSans", 30)
     report.drawCentredString(width/2, 725*height/800, getCompanyName(company))
     report.setFont("HumorSans", 17)
     report.drawCentredString(width/2, 700*height/800, "("+company+")")
     report.line(width/32, 43*height/50, 31*width/32, 43*height/50)
     report.line(width/16, 42*height/50, 15*width/16, 42*height/50)
+
+
     print("Creating report on "+company)
+
+    #Generating .pngs of the predictions from the Neural Net and Linear approximation, respectively
     predictedPrice = graphNN(company, '11/24/15', runs, True)[0]
     graphRecentTrend(company)
+
+    #Put the .pngs on the report, below the aesthetic lines of the header. NN on the right, linear on the left
     report.drawImage(company+"NN.png", 4.8*width/10, 36*height/80, height=300, width=300, mask='auto')
     report.drawImage(company+"linear.png", 1*width/10, 42*height/80, height=200, width=200, mask='auto')
     report.setFont("HumorSans", 25)
@@ -58,8 +70,6 @@ def genReport(company, runs, newsCount):
         #report.drawCentredString(4*width/10, height/5, summarize(newsUrls[1]))
     report.showPage()
     report.save()
-
-def createHeader(ticker):
 
 def getCompanyName(ticker):
     yahoo = requests.get("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="+ticker+"&region=1&lang=en")
