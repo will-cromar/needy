795a1e5451e24372215d697a644539c0dd240a1c
will-cromar <wcromar@knights.ucf.edu>
2015-12-03 22:08:51 -0500

    Removed an unused function

--- price_parsing.py
+++ price_parsing.py
@@ -30,27 +30,6 @@ def getStockPrices(ticker, frequency="monthly", update=False):
     # Return closing prices
     return prices.get("Close")
 
-# TODO: Merge with getStockPrices
-def getStockPriceDifferentials(ticker, frequency="monthly", update=False):
-    """
-    Gets the percent differential of the closing prices for a given ticker
-    :param ticker: Name of the company's ticker
-    :param frequency: Frequency of returned time series. See Quandl.get()'s collapse param.
-    :param update: Always updates instead of using cache if true
-    :return: Pandas dataframe representing a time series of the closing price differentials for a ticker
-    """
-    name = ticker + "_differentials_" + frequency # Name of data in cache
-    prices = None
-    # If there is no cached version of the pickle, or update flag is on, download price data and cache it
-    if update or not util.pickleExists(name):
-        prices = Quandl.get(STOCK_DATA_SOURCE + ticker, collapse=frequency, transform="rdiff", authtoken="xx_T2u2fsQ_MjyZjTb6E")
-        util.savePickle(prices, name)
-    # Otherwise, use most recent cache entry
-    else:
-        prices = util.getMostRecentPickle(name)
-
-    return prices.get("Close")
-
 def getDateRange(prices, before, after):
     """
     Takes a pandas dataframe and truncates it to include only dates between before and after
